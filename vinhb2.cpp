bee0bf4818d66fc62e03b82d6f1902c96c72c7b7
tran huy tung <tranhuytunga1k51nl@gmail.com>
2024-05-23 18:36:56 +0700

    baitap TCP ko co buffer

--- vinhb2.cpp
+++ vinhb2.cpp
@@ -1,113 +1,77 @@
 #include<bits/stdc++.h>
 using namespace std;
+class host{
+    public: int seq, ack;
+    public: int buff,previous;
+    public: string name;
+};
 int main(){
-    set<char> tmp;
-    set<char> U={'a','b','c','d','e','f','g','h'};
-    // cout<<U.size();
-    set<char> A={'a','b','f','h','k'};
-    set<char> B={'b','g','h'};
-    cout<<"A: ";
-    for(auto x: U){
-      if(A.count(x)>0) cout<<1;
-      else cout<<0;
+    host A,B;
+    A.name="A";     B.name="B";
+    int step,MSG;
+    // set value seq,ack
+    A.seq=1000;     B.seq=1000;  //demo
+    A.ack=0;        B.ack=1000;
+    cout<<"STEP: ";
+    cin>>step;
+    cout<<"MGS: ";
+    cin>>MSG;
+    int i=1;
+    host *from,*to;
+    while (step--){
+        string input;
+        
+        bool finish,PhatHien_wrongMsg=0,acceptMSG;
+        cout<<"FINISH ?";
+        cin>>finish;
+        cout<<"A to B || B to A: ";
+        cin>>input;
+        if(input=="A") {
+          from=&A;
+          to=&B;
+        }
+        else {
+          from=&B;
+          to=&A;
+        }
+        if(i==1) {
+          from->ack=to->seq;
+          from->previous=0;
+          to->previous=0;
+          acceptMSG=1;
+        }
+        cout<<"step "<<i<<": SEQ_"<<from->name<<": "<<from->seq<<"   ACK_"+from->name<<": "<<from->ack;
+        if(PhatHien_wrongMsg==1) cout<<"PHAT HIEN GUI SAI"<<endl;
+        else cout<<endl;
+        if(acceptMSG==0) cout<<" KO ACC"<<endl;
+        else cout<<endl;
+        if (from->seq != to->ack) acceptMSG=0;
+        else acceptMSG=1;
+        if (acceptMSG==0 && from->previous == 0)
+          PhatHien_wrongMsg = 1;
+        else
+          PhatHien_wrongMsg = 0;
+        if(finish==1){
+            if(acceptMSG==1){
+              from->seq+=MSG;
+              to->ack+=MSG;
+            }
+            else{
+              if(PhatHien_wrongMsg==0){
+                from->seq+=MSG;
+              }
+              else{
+                from->seq=to->ack;
+                to->seq=from->ack;
+              }
+            }  
+        }
+        else{
+          from->seq+=MSG;
+        }
+        from->previous=1;
+        to->previous=0;
+        i++;
     }
-    cout<<endl; 
-    // ---------------------------------------
-    cout<<"B: ";
-    for(auto x: U){
-      if(B.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"A giao B: ";
-    for(auto x: A){
-      if(B.count(x)>0) tmp.insert(x);
-    }
-    for(auto x: tmp) cout<<x<<" ";
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"B giao A: ";
-    for(auto x: B){
-      if(A.count(x)>0) tmp.insert(x);
-    }
-    for(auto x: tmp) cout<<x<<" ";
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"A hop B: ";
-    for(auto x: A){
-      tmp.insert(x);
-    }
-    for(auto x: B){
-      tmp.insert(x);
-    }
-    for(auto x: tmp) cout<<x<<" ";
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"B hop A: ";
-    for(auto x: B){
-      tmp.insert(x);
-    }
-    for(auto x: A){
-      tmp.insert(x);
-    }
-    for(auto x: tmp) cout<<x<<" ";
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"A/B: ";
-    for(auto x: A){
-      if(B.count(x)==0) tmp.insert(x);
-    }
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"B/A: ";
-    for(auto x: B){
-      if(A.count(x)==0) tmp.insert(x);
-    }
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    tmp.clear();
-    cout<<endl;
-    // ---------------------------------------
-    cout<<"A tam giac B: ";
-    for(auto x: A){
-      if(B.count(x)==0) tmp.insert(x);
-    }
-    for(auto x: B){
-      if(A.count(x)==0) tmp.insert(x);
-    }
-    for(auto x: tmp) cout<<x<<" ";
-    for(auto x: U){
-      if(tmp.count(x)>0) cout<<1;
-      else cout<<0;
-    }
-    cout<<endl;
-    tmp.clear();
+    
 }
